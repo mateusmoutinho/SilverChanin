07f539b2bdd87908cccc473b5c9b1ea0eaae1f95
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-06 16:39:42 -0300

    att

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf release
 mkdir release
-./silverchain.o --tags api_dependencies  api_const  api_type api_declare api_define cli_dependencies cli_type cli_globals  cli_declare cli_define --src src --project_short_cut  SilverChain
+./silverchain.o --tags api_dependencies  api_const  api_type api_declare api_define cli_dependencies cli_consts cli_type cli_globals  cli_declare cli_define --src src --project_short_cut  SilverChain
 ./CAmalgamator.o --file   src/cli/main.c -o release/SilverChain.c
 ./CAmalgamator.o --file src/imports/imports.api_define.h  -o release/SilverChainApiOne.h
 ./CAmalgamator.o --file  src/imports/imports.api_define.h  -o release/CSilverChainApiNoDependenciesIncluded.h \
